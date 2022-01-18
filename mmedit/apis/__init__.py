7276fb396031de0486ed7f2ee9874803c7fb2890
KheiynCube <chunchat02@gmail.com>
2022-01-18 20:52:31 +0700

    add: inpainting inference 2

--- mmedit/apis/__init__.py
+++ mmedit/apis/__init__.py
@@ -1,6 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .generation_inference import generation_inference
 from .inpainting_inference import inpainting_inference
+from .inpainting_inference2 import inpainting_inference2
 from .matting_inference import init_model, matting_inference
 from .restoration_face_inference import restoration_face_inference
 from .restoration_inference import restoration_inference
@@ -10,7 +11,9 @@ from .train import set_random_seed, train_model
 
 __all__ = [
     'train_model', 'set_random_seed', 'init_model', 'matting_inference',
-    'inpainting_inference', 'restoration_inference', 'generation_inference',
+    'inpainting_inference', 
+    'inpainting_inference2', 
+    'restoration_inference', 'generation_inference',
     'multi_gpu_test', 'single_gpu_test', 'restoration_video_inference',
     'restoration_face_inference'
 ]
---
+++ mmedit/apis/inpainting_inference2.py
@@ -0,0 +1,119 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import torch
+import mmcv
+from mmcv.parallel import collate, scatter
+
+from mmedit.datasets.pipelines import Compose
+
+# class LoadImage:
+#     """A simple pipeline to load image."""
+
+#     def __call__(self, results):
+#         """Call function to load images into results.
+
+#         Args:
+#             results (dict): A result dict contains the file name
+#                 of the image to be read.
+
+#         Returns:
+#             dict: ``results`` will be returned containing loaded image.
+#         """
+
+#         if isinstance(results['masked_img'], str):
+#             results['filename'] = results['masked_img']
+#             results['ori_filename'] = results['masked_img']
+#         else:
+#             results['filename'] = None
+#             results['ori_filename'] = None
+#         img = mmcv.imread(results['masked_img'])
+#         results['masked_img'] = img
+#         results['img_shape'] = img.shape
+#         results['ori_shape'] = img.shape
+#         return results
+
+
+class LoadImage:
+    """A simple pipeline to load image."""
+
+    def __call__(self, results):
+        """Call function to load images into results.
+
+        Args:
+            results (dict): A result dict contains the file name
+                of the image to be read.
+
+        Returns:
+            dict: ``results`` will be returned containing loaded image.
+        """
+
+        if isinstance(results['masked_img'], str):
+            results['filename'] = results['masked_img']
+            results['ori_filename'] = results['masked_img']
+        else:
+            results['filename'] = None
+            results['ori_filename'] = None
+        img = mmcv.imread(results['masked_img'])
+        results['masked_img'] = img
+        results['img_shape'] = img.shape
+        results['ori_shape'] = img.shape
+        return results
+
+class LoadMask:
+    """A simple pipeline to load image."""
+
+    def __call__(self, results):
+        """Call function to load images into results.
+
+        Args:
+            results (dict): A result dict contains the file name
+                of the image to be read.
+
+        Returns:
+            dict: ``results`` will be returned containing loaded image.
+        """
+
+        mask = mmcv.imread(results['mask'])
+        results['mask'] = mask
+        return results
+
+def inpainting_inference2(model, masked_img, mask):
+    """Inference image with the model.
+
+    Args:
+        model (nn.Module): The loaded model.
+        masked_img (str): File path of image with mask.
+        mask (str): Mask file path.
+
+    Returns:
+        Tensor: The predicted inpainting result.
+    """
+    device = next(model.parameters()).device  # model device
+
+    infer_pipeline = [
+        dict(type='Pad', keys=['masked_img', 'mask'], mode='reflect'),
+        dict(
+            type='Normalize',
+            keys=['masked_img'],
+            mean=[127.5] * 3,
+            std=[127.5] * 3,
+            to_rgb=False),
+        dict(type='GetMaskedImage', img_name='masked_img'),
+        dict(
+            type='Collect',
+            keys=['masked_img', 'mask'],
+            meta_keys=['masked_img']),
+        dict(type='ImageToTensor', keys=['masked_img', 'mask'])
+    ]
+
+    # build the data pipeline
+    infer_pipeline = [LoadImage(), LoadMask()] + infer_pipeline
+    test_pipeline = Compose(infer_pipeline)
+    # prepare data
+    data = dict(masked_img=masked_img, mask=mask)
+    data = test_pipeline(data)
+    data = scatter(collate([data], samples_per_gpu=1), [device])[0]
+    # forward the model
+    with torch.no_grad():
+        result = model(test_mode=True, **data)
+
+    return result['fake_img']
