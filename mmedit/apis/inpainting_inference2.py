d0680cdffad0648bad4511be768c943301a9bae7
KheiynCube <chunchat02@gmail.com>
2022-01-19 18:00:44 +0700

    add: allow cpu

--- mmedit/apis/inpainting_inference2.py
+++ mmedit/apis/inpainting_inference2.py
@@ -5,32 +5,6 @@ from mmcv.parallel import collate, scatter
 
 from mmedit.datasets.pipelines import Compose
 
-# class LoadImage:
-#     """A simple pipeline to load image."""
-
-#     def __call__(self, results):
-#         """Call function to load images into results.
-
-#         Args:
-#             results (dict): A result dict contains the file name
-#                 of the image to be read.
-
-#         Returns:
-#             dict: ``results`` will be returned containing loaded image.
-#         """
-
-#         if isinstance(results['masked_img'], str):
-#             results['filename'] = results['masked_img']
-#             results['ori_filename'] = results['masked_img']
-#         else:
-#             results['filename'] = None
-#             results['ori_filename'] = None
-#         img = mmcv.imread(results['masked_img'])
-#         results['masked_img'] = img
-#         results['img_shape'] = img.shape
-#         results['ori_shape'] = img.shape
-#         return results
-
 
 class LoadImage:
     """A simple pipeline to load image."""
@@ -87,7 +61,6 @@ def inpainting_inference2(model, masked_img, mask):
     Returns:
         Tensor: The predicted inpainting result.
     """
-    device = next(model.parameters()).device  # model device
 
     infer_pipeline = [
         dict(type='Pad', keys=['masked_img', 'mask'], mode='reflect'),
@@ -111,7 +84,17 @@ def inpainting_inference2(model, masked_img, mask):
     # prepare data
     data = dict(masked_img=masked_img, mask=mask)
     data = test_pipeline(data)
-    data = scatter(collate([data], samples_per_gpu=1), [device])[0]
+
+    # data = scatter(collate([data], samples_per_gpu=1), [device])[0]
+
+    data = collate([data], samples_per_gpu=1)
+    if next(model.parameters()).is_cuda:
+        # scatter to specified GPU
+        device = next(model.parameters()).device  # model device
+        data = scatter(data, [device])[0]
+    else:
+        data = scatter(data, [-1])[0]
+
     # forward the model
     with torch.no_grad():
         result = model(test_mode=True, **data)
